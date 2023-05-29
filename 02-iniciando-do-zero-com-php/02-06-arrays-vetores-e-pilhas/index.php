3ace385485d607841afdd4d7ec8074f65e1aabba
Fernando <death_dan@hotmail.com>
2023-05-29 10:29:10 -0300

    Arrays Vetores e Pilhas

--- 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
+++ 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
@@ -6,22 +6,100 @@ fullStackPHPClassName("02.06 - Arrays, vetores e pilhas");
  * [ arrays ] https://php.net/manual/pt_BR/language.types.array.php
  */
 fullStackPHPClassSession("index array", __LINE__);
+$arrA = array(1, 2, 3);
+$arrA = [0, 1, 2, 3];
+var_dump($arrA);
 
+$arrayIndex = [
+    "Brain",
+    "Angus",
+    "Malcom"
+];
+
+$arrayIndex[] = "Cliff";
+$arrayIndex[] = "Phil";
+
+var_dump($arrayIndex);
 
 /**
  * [ associative array ] "key" => "value"
  */
 fullStackPHPClassSession("associative array", __LINE__);
 
+$arrayAssoc = [
+    "vocal" => "Brian",
+    "solo_guitar" => "Angus",
+    "base_guitar" => "Malcom",
+    "bass_guitar" => "Cliff"
+];
+
+$arrayAssoc["drums"] = "Phil";
+$arrayAssoc["rock_band"] = "AC/DC";
+var_dump($arrayAssoc);
 
 /**
  * [ multidimensional array ] "key" => ["key" => "value"]
  */
 fullStackPHPClassSession("multidimensional array", __LINE__);
 
+$brain = ["Brain", "Mic"];
+$angus = ["name" => "Angus", "instrument" => "Guitar"];
+
+$instrument = [
+    $brain,
+    $angus
+];
+var_dump($instrument);
+
+var_dump([
+    "brian" => $brain,
+    "angus" => $angus
+]);
 
 /**
  * [ array access ] foreach(array as item) || foreach(array as key => value)
  */
 fullStackPHPClassSession("array access", __LINE__);
+
+$acdc = [
+    "band" => "AC/DC",
+    "vocal" => "Brian",
+    "solo_guitar" => "Angus",
+    "base_guitar" => "Malcom",
+    "bass_guitar" => "Cliff",
+    "drums" => "Phil"
+];
+
+echo "<p>Vocal da Banda AC/DC é {$acdc["vocal"]}, junto com o Guitarrista {$acdc["solo_guitar"]} fazem um show incrivel!!!</p>";
+
+$pearl_jam = [
+    "band" => "Pearl Jam",
+    "vocal" => "Eddie",
+    "solo_guitar" => "Mike",
+    "base_guitar" => "Stone",
+    "bass_guitar" => "Jeff",
+    "drums" => "Jack"
+];
+
+$rockBands = [
+    "acdc" => $acdc,
+    "pearl" => $pearl_jam
+];
+
+var_dump($rockBands);
+
+echo "<p>{$rockBands['pearl']['vocal']}</p>";
+
+foreach ($acdc as $item) {
+    echo "<p>{$item}</p>";
+}
+
+foreach ($acdc as $key => $value) {
+    echo "<p>{$value} is a {$key} of band!</p>";
+}
+
+foreach ($rockBands as $rockBand) {
+    $art = "<article><h1>%s</h1><p>%s</p><p>%s</p><p>%s</p><p>%s</p><p>%s</p></article>";
+    vprintf($art, $rockBand);
+}
 
