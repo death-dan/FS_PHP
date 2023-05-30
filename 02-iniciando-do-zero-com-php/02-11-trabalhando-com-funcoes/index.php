a2964cb665c208346bc085e3c06f430f440f5926
Fernando <death_dan@hotmail.com>
2023-05-29 23:03:28 -0300

    Trabalhando com Funções

--- 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
+++ 02-iniciando-do-zero-com-php/02-11-trabalhando-com-funcoes/index.php
@@ -6,21 +6,39 @@ fullStackPHPClassName("02.11 - Trabalhando com funções");
  * [ functions ] https://php.net/manual/pt_BR/language.functions.php
  */
 fullStackPHPClassSession("functions", __LINE__);
+include __DIR__ . "/functions.php";
 
+var_dump(functionName("Bob Marley", "Pantera", "Slayer"));
+var_dump(functionName("Bob", "Thommas", "Homer"));
+
+var_dump(optionArgs("Jose"));
+var_dump(optionArgs("Antonio", "Homer"));
+var_dump(optionArgs("Silver", "Gold", "Platina"));
 
 /*
  * [ global access ] global $var
  */
 fullStackPHPClassSession("global access", __LINE__);
 
+$weight = 80;
+$height = 1.70;
+
+echo calcImc();
 
 /*
  * [ static arguments ] static $var
  */
 fullStackPHPClassSession("static arguments", __LINE__);
 
+$pay = payTotal(300);
+$pay = payTotal(1300);
+$pay = payTotal(3000);
+
+echo $pay;
 
 /*
  * [ dinamic arguments ] get_args | num_args
  */
 fullStackPHPClassSession("dinamic arguments", __LINE__);
+
+var_dump(myTeam("Jose", "Maria", "Joao", "Oliveira"));
