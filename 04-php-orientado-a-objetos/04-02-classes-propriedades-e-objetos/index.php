02e41bbeb0b1dc0e08aef581672c7f78f40862a7
Fernando <death_dan@hotmail.com>
2023-06-09 19:20:26 -0300

    Classes, Propriedades e Objetos

--- 04-php-orientado-a-objetos/04-02-classes-propriedades-e-objetos/index.php
+++ 04-php-orientado-a-objetos/04-02-classes-propriedades-e-objetos/index.php
@@ -7,14 +7,37 @@ fullStackPHPClassName("04.02 - Classes, propriedades e objetos");
  */
 fullStackPHPClassSession("classe e objeto", __LINE__);
 
+include __DIR__ . "/classes/User.php";
+
+$user = new User();
+var_dump($user);
 
 /*
  * [ propriedades ] http://php.net/manual/pt_BR/language.oop5.properties.php
  */
 fullStackPHPClassSession("propriedades", __LINE__);
 
+$user->firstName = "José";
+$user->lastName = "Maria";
+$user->email = "josemaria@gcloves.com";
+
+var_dump($user);
+echo "<p>Eu so {$user->firstName} {$user->lastName}</p>";
+
+echo "<p>Nome: {$user->getFirstName()} {$user->getLastName()} <br> E-mail: {$user->getEmail()}</p>";
 
 /*
  * [ métodos ] São as funções que definem o comportamento e a regra de negócios de uma classe
  */
 fullStackPHPClassSession("métodos", __LINE__);
+
+$user->setFirstName("Maria");
+$user->setLastName("José");
+
+if (!$user->setEmail("mariajose@cv.com")) {
+    echo "<p class='trigger error'>O e-mail {$user->getEmail()} não é válido!</p>";
+} else {
+    echo "<p class='trigger accept'>O e-mail {$user->getEmail()} é válido! Bem Vindo!!</p>";
+}
+
+var_dump($user);
