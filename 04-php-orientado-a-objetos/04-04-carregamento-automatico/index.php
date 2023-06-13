a88c364a040f36f2e2ca8895332099a194355064
Fernando <death_dan@hotmail.com>
2023-06-13 16:28:37 -0300

    Carregamento Automatico

--- 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
@@ -6,9 +6,37 @@ fullStackPHPClassName("04.04 - Carregamento automático");
  * [ autoload spl psr-4 ] Carregamento de suas classes com spl_autoload psr-4
  */
 fullStackPHPClassSession("autoload spl psr-4", __LINE__);
+// require __DIR__ . "/source/Loading/User.php";
+// require __DIR__ . "/source/Loading/Address.php";
+// require __DIR__ . "/source/Loading/Company.php";
 
+require __DIR__ . "/source/autoload.php";
+
+$user = new \Source\Loading\User();
+$addr = new \Source\Loading\Address();
+$company = new \Source\Loading\Company();
+
+var_dump(
+    $user,
+    $addr,
+    $company
+);
 
 /*
  * [ autoload composer psr-4 ] https://getcomposer.org/doc/00-intro.md
  */
 fullStackPHPClassSession("autoload composer psr-4", __LINE__);
+
+
+require __DIR__ . "/vendor/autoload.php";
+
+$user = new \Source\Loading\User();
+$addr = new \Source\Loading\Address();
+$company = new \Source\Loading\Company();
+
+var_dump(
+    $user,
+    $addr,
+    $company
+);
+
---
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '8bc8c5d7b6035114fb88dbc310af4652c3c5a711',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8bc8c5d7b6035114fb88dbc310af4652c3c5a711',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'coffeecode/uploader' => array(
+            'pretty_version' => '1.0.11',
+            'version' => '1.0.11.0',
+            'reference' => '182682c029ff23d28aa8f5d7db2db4c060fc3ae4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../coffeecode/uploader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
