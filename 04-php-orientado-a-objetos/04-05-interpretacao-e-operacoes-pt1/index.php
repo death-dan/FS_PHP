6e5e79fe9fa6f42d70b860f73661df1dc5e33b5c
Fernando <death_dan@hotmail.com>
2023-06-14 08:13:06 -0300

    Interpretações e Operações pt1

--- 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
+++ 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
@@ -10,12 +10,35 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("__construct", __LINE__);
 
+$user = new \Source\Interpretation\User(
+    "Jose",
+    "Maria",
+    "blabls@bla.com"
+);
+
+var_dump($user);
 
 /*
  * [ clone ] Executado automaticamente quando um novo objeto é criado a partir do operador clone.
  * http://php.net/manual/pt_BR/language.oop5.cloning.php
  */
 fullStackPHPClassSession("__clone", __LINE__);
+
+$maria = $user;
+$maria->setFirstName("Maria");
+$maria->setLastName("Mercedes");
+
+$jonas = clone $maria;
+
+$jonasC = clone $maria;
+$jonasC->setFirstName("Jonas");
+$jonasC->setLastName("Jose");
+
+var_dump(
+    $maria,
+    $jonas,
+    $jonasC
+);
 
 
 /*
