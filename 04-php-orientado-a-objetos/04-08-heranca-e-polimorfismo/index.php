942db1199cb68d83116b2626585bd440db007925
Fernando <death_dan@hotmail.com>
2023-06-14 23:15:36 -0300

    Herança e Polimorfismo

--- 04-php-orientado-a-objetos/04-08-heranca-e-polimorfismo/index.php
+++ 04-php-orientado-a-objetos/04-08-heranca-e-polimorfismo/index.php
@@ -10,12 +10,42 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("classe pai", __LINE__);
 
+$event =  new \Source\Inheritance\Event\Event(
+    "WorkFlow",
+    new DateTime("2023-09-20 16:00"),
+    200,
+    "4"
+);
+
+var_dump($event);
+
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
 
 /*
  * [ classe filha ] Uma classe que herda a classe pai e especializa seuas rotinas
  */
 fullStackPHPClassSession("classe filha", __LINE__);
 
+$address = new \Source\Inheritance\Address("Rua do Brás", "666");
+$event =  new \Source\Inheritance\Event\EventLive(
+    "WorkFlow",
+    new DateTime("2023-09-20 16:00"),
+    200,
+    "4",
+    $address
+);
+
+var_dump($event);
+
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
 
 /*
  * [ polimorfismo ] Uma classe filha que tem métodos iguais (mesmo nome e argumentos) a class
@@ -23,3 +53,17 @@ fullStackPHPClassSession("classe filha", __LINE__);
  */
 fullStackPHPClassSession("polimorfismo", __LINE__);
 
+$event =  new \Source\Inheritance\Event\EventOnline(
+    "WorkFlow",
+    new DateTime("2023-09-20 16:00"),
+    200,
+    "https://localhost"
+);
+
+var_dump($event);
+
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
+$event->register("Jose", "blabla@bla.com");
