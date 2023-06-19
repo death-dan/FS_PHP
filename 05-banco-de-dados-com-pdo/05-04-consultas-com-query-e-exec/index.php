cd221bf88fa4a7936f82afc16ab30ee6f58dff9f
Fernando <death_dan@hotmail.com>
2023-06-19 12:53:38 -0300

    Consultas com Query e Exec

--- 05-banco-de-dados-com-pdo/05-04-consultas-com-query-e-exec/index.php
+++ 05-banco-de-dados-com-pdo/05-04-consultas-com-query-e-exec/index.php
@@ -4,6 +4,7 @@ fullStackPHPClassName("05.04 - Consultas com query e exec");
 
 require __DIR__ . "/../source/autoload.php";
 
+use Source\DataBase\Conect;
 use Source\Database\Connect;
 
 /*
@@ -14,7 +15,27 @@ use Source\Database\Connect;
  * [ PDO query ]http://php.net/manual/pt_BR/pdo.query.php
  */
 fullStackPHPClassSession("insert", __LINE__);
+$insert = "
+    INSERT INTO users (first_name, last_name, email, document)
+    VALUES ('Jose', 'Maria', 'blabla@bla.com', '09876565');
+";
 
+try {
+    // $exec = Conect::getInstance()->exec($insert);
+    // var_dump(Conect::getInstance()->lastInsertId());
+    $exec = null;
+
+    $query = Conect::getInstance()->query($insert);
+    var_dump(Conect::getInstance()->lastInsertId());
+
+    var_dump(
+        $exec,
+        $query->errorInfo()
+    );
+
+} catch (PDOException $e) {
+    var_dump($e);
+}
 
 /*
  * [ select ] Ler/Consultar dados.
@@ -22,6 +43,16 @@ fullStackPHPClassSession("insert", __LINE__);
  */
 fullStackPHPClassSession("select", __LINE__);
 
+try {
+    $query = Conect::getInstance()->query("SELECT * FROM users LIMIT 2");
+    var_dump([
+        $query,
+        $query->rowCount(),
+        $query->fetchAll()
+    ]);
+} catch (PDOException $e) {
+    var_dump($e);
+}
 
 /*
  * [ update ] Atualizar dados.
@@ -29,9 +60,28 @@ fullStackPHPClassSession("select", __LINE__);
  */
 fullStackPHPClassSession("update", __LINE__);
 
+try {
+    $exec = Conect::getInstance()->exec("
+        UPDATE users SET first_name = 'Jose', last_name='Maria' 
+        WHERE id='1'
+    ");
+    var_dump($exec);
+
+} catch (PDOException $e) {
+    var_dump($e);
+}
 
 /*
  * [ delete ] Deletar dados.
  * https://mariadb.com/kb/en/library/delete/
  */
 fullStackPHPClassSession("delete", __LINE__);
+
+try {
+    $exec = Conect::getInstance()->exec("
+        DELETE FROM users WHERE id > '50'
+    ");
+    var_dump($exec);
+} catch (PDOException $e) {
+    var_dump($e);
+}
