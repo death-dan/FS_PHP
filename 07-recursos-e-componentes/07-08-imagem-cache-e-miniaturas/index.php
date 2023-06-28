fb5348c2666c22b0d28827f42d9cca97ca3dab3c
Fernando <death_dan@hotmail.com>
2023-06-28 06:15:43 -0300

    Imagem, Cache e Miniaturas

--- 07-recursos-e-componentes/07-08-imagem-cache-e-miniaturas/index.php
+++ 07-recursos-e-componentes/07-08-imagem-cache-e-miniaturas/index.php
@@ -9,8 +9,21 @@ require __DIR__ . "/../vendor/autoload.php";
  */
 fullStackPHPClassSession("cropper", __LINE__);
 
+$t = new \Source\Support\Thumb();
+var_dump($t);
 
 /*
  * [ generate ]
  */
 fullStackPHPClassSession("generate", __LINE__);
+
+echo "<img src='{$t->make("files/2023/06/images.jpg", 300)}' alt='' title='' />";
+echo "<img src='{$t->make("files/2023/06/images.jpg", 180, 180)}' alt='' title='' />";
+
+var_dump($t->make("image.jpg", 100));
+
+echo "<img src='{$t->make("files/2023/06/download.png", 300)}' alt='' title='' />";
+echo "<img src='{$t->make("files/2023/06/download.png", 180, 180)}' alt='' title='' />";
+
+// $t->flush("files/2023/06/download.png");
+
